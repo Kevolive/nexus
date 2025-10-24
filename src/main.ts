c416f28f637f5c8342d494cbd5261fbfe9f7c468
Kevin Olivella <kevinolivella@gmail.com>
2025-10-23 21:23:04 -0500

    Se modifica el main.js agregando el uso de cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors({
+    origin: ['http://localhost:4200', 'https://nexus-nwgg.onrender.com'],
+    credentials: true,
+  }));
 
- 
   app.enableCors({
     origin: [
       'http://localhost:4200',
@@ -23,3 +27,7 @@ async function bootstrap() {
   console.log(`✅ App corriendo en puerto ${port}`);
 }
 bootstrap();
+function cors(arg0: { origin: string[]; credentials: boolean; }): any {
+  throw new Error('Function not implemented.');
+}
+
