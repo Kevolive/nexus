9ee0170fecf7ba07ce0b990dedc209a9d8c31831
Kevin Olivella <kevinolivella@gmail.com>
2025-10-23 21:20:19 -0500

    modifico el main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { swaggerConfig } from './config/swagger.config'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+ 
   app.enableCors({
     origin: [
       'http://localhost:4200',
       'https://nexus-nwgg.onrender.com'
     ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization' ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
   });
 
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  if (process.env.NODE_ENV !== 'production') {
-    swaggerConfig(app);
-    console.log('Swagger en: ', process.env.SWAGGER_PATH || 'api/docs');
-    
-  }
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
-  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`✅ App corriendo en puerto ${port}`);
 }
 bootstrap();
